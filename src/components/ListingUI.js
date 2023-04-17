1c32e2b27988b4560beb846a66354bc73c76a7db
Sweety Santhosh <ss9334464@gmail.com>
2023-04-17 20:42:58 +0400

    Update ListingUI.js
    second

--- src/components/ListingUI.js
+++ src/components/ListingUI.js
@@ -6,7 +6,7 @@ const ListingUI = () => {
     { id: 2, title: 'Item 2', description: 'Description for Item 2' },
     { id: 3, title: 'Item 3', description: 'Description for Item 3' },
     { id: 4, title: 'Item 4', description: 'Description for Item 4' },
-    { id: 5, title: 'Item 5', description: 'Description for Items 5' }
+    { id: 5, title: 'Item 5', description: 'Description for Itemsss 5' }
   ];
 
   return (
